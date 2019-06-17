0c0655a9d1d65f4c6da3c80013364bdfdab9c9f3
XiaoCai20180701 <421586311@qq.com>
2019-06-17 16:08:36 +0800

    Initialization commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <i-button type="primary" class="txt">我是童e家</i-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.txt {
+  margin-top: 50px;
+  font-size: 46px;
+}
+</style>
