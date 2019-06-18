748618cbcdd5497e656cbe9f2487e56a44184801
XiaoCai20180701 <421586311@qq.com>
2019-06-18 16:46:13 +0800

    添加导航守卫，使用 router.beforeEach 注册一个全局前置守卫，判断用户是否登陆

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/pages/login/Login'
 
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/hello',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    next()
+  } else {
+    let token = localStorage.getItem('token');
+    
+    if (token === 'null' || token === '') {
+      next('/login')
+    } else {
+      next()
+    }
+  }
+})
+
+export default router
