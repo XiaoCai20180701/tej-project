5842ae1105848e3ffc0478414bff2482e508f62d
XiaoCai20180701 <421586311@qq.com>
2019-06-27 17:35:17 +0800

    页面头部 init commit

---
+++ src/components/layout/headTop.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="layout-header">
+    我是头部
+    <!--<i-button type="text" @click="toggleClick">-->
+      <!--<Icon type="navicon" size="32"></Icon>-->
+    <!--</i-button>-->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'headTop',
+    data(){
+      return {
+      }
+    },
+    created(){
+    },
+    computed: {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style>
+  .header_container{
+    background-color: #EFF2F7;
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 20px;
+  }
+  .avator{
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    margin-right: 37px;
+  }
+  .el-dropdown-menu__item{
+    text-align: center;
+  }
+</style>
