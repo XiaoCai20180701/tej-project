1de74eb8f222484d00ffd8c1d598d9168382bafc
XiaoCai20180701 <421586311@qq.com>
2019-06-27 17:37:43 +0800

    修改路由加载方式为 懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Login from '@/pages/login/Login'
+
+const login = r => require.ensure([], () => r(require('@/pages/login')), 'login')
+const layout = r => require.ensure([], () => r(require('@/components/layout/layout')), 'layout')
+const hello = r => require.ensure([], () => r(require('@/components/helloWorld')), 'helloWorld')
+const index = r => require.ensure([], () => r(require('@/pages/index')), 'index')
 
 Vue.use(Router)
 
-const router =  new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/hello',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+const routes = [
+  {
+    path: '/',
+    component: login
+  },
+  {
+    path: '/manage',
+    component: layout,
+    name: '',
+    children: [
+      {
+        path: '/hello',
+        component: hello,
+        meta: ['订单管理','hello']
+      },
+      {
+        path: '/index',
+        component: index,
+        meta: ['订单管理','index']
+      }
+    ]
+  },
+]
 
-router.beforeEach((to, from, next) => {
-  if (to.path === '/login') {
-    next()
-  } else {
-    let token = localStorage.getItem('token');
-    
-    if (token === 'null' || token === '') {
-      next('/login')
-    } else {
-      next()
-    }
-  }
-})
+// router.beforeEach((to, from, next) => {
+//   if (to.path === '/login') {
+//     next()
+//   } else {
+//     let token = localStorage.getItem('token');
+//
+//     if (token === 'null' || token === '') {
+//       next('/login')
+//     } else {
+//       next()
+//     }
+//   }
+// })
 
-export default router
+export default new Router({
+  routes,
+  strict: process.env.NODE_ENV !== 'production',
+})
