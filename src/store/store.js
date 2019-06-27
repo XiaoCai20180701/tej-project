c9df5da8e1f9f67838de20fea45a10d397e87c2b
XiaoCai20180701 <421586311@qq.com>
2019-06-27 17:38:31 +0800

    将登录成功返回的菜单权限列表存放在vuex

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,43 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-//import mutations from './mutations';
-//import actions from './actions'
+import Vue from 'vue'
+import Vuex from 'vuex'
+// import mutations from './mutations'
+// import actions from './actions'
+// import getters from './getters'
+// import state from './state'
+import { getLogin } from '@/api/api'
 
 Vue.use(Vuex);
 
 const key = 'user'
+
 const state = {
-  token: localStorage.getItem('token') ? localStorage.getItem('token') : ''
-};
+  token: localStorage.getItem('token') ? localStorage.getItem('token') : '',
+  userInfo: {},
+  menuList: []
+}
 
 const mutations = {
   //修改token，并将token存入localStorage
   changeLogin(state, user) {
     state.token = user.token
     localStorage.setItem('token',user.token)
+  },
+  GetMenuList(state,menuList){
+    state.menuList = menuList
+    console.log('mutations menuList',state.menuList)
   }
 }
 
-const actions = {}
+const actions = {
+  async getMenuListData({commit},playload) {
+    try {
+      const res = await getLogin(playload.username,playload.password)
+      commit('GetMenuList',res.data)
+    }catch(err){
+      console.log(err.message)
+    }
+  }
+}
 const store = new Vuex.Store({
   state,
   mutations,
