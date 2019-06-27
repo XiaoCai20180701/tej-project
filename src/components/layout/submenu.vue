c8dfb429a0893883b030a4cb83ac2674dd176c47
XiaoCai20180701 <421586311@qq.com>
2019-06-27 17:36:19 +0800

    左侧菜单二级菜单 init commit

---
+++ src/components/layout/submenu.vue
@@ -0,0 +1,32 @@
+<template>
+    <Submenu :name="title">
+      <template slot="title">
+        <Icon type="ios-navigate"></Icon>
+        {{title}}
+      </template>
+      <MenuItem v-for="(sub,m) in submenu.submenu_list" :name="id + '-' + m" :key="id + '-' + m">
+        {{sub.name}}
+      </MenuItem>
+    </Submenu>
+</template>
+
+<script>
+  export default {
+    name: 'submenu',
+    props: {
+      id: String,
+      title: String,
+      submenu: Object
+    },
+    data(){
+      return {
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
+
+
