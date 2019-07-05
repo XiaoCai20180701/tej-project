595765faabdb58bd1b5dcf33a2c6d4c46d0f6e6d
XiaoCai20180701 <421586311@qq.com>
2019-07-05 16:30:19 +0800

    设置菜单路由，将其存储在vuex  init commit

---
+++ src/store/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { getRes } from '../api/api';
+import  frontRoutes  from '@/router/frontRoutes'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        // 左侧菜单栏数据 (前端路由表)
+        menuItems: frontRoutes,
+        role: 0
+    },
+    mutations: {
+        SET_MENUS(state, items) {
+          state.menuItems = [...items]
+        },
+        SET_ROLE(state, role){
+            state.role = role
+        }
+    },
+    actions: {
+        getAnyscMenu({commit},role){
+            getRes(role).then(res => {
+                let menuItems = res.data.menuItems
+                commit('SET_MENUS',menuItems)
+            }).catch(err => {
+                console.log('get res fail',err)
+            })
+        },
+        getRole({commit},role){
+          commit('SET_ROLE',role)
+        }
+    }
+
+})
+
+export default store
