6f082a29b0009b1ce2db641f92bec0da527f0502
XiaoCai20180701 <421586311@qq.com>
2019-07-05 16:19:54 +0800

    商品管理 => 未发货 页面 init commit

---
+++ src/pages/OrderManagement/Unshipped.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Unshipped</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Unshipped'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
