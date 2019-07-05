ce940ca77ecaca08b9198b59ead17a8db250484f
XiaoCai20180701 <421586311@qq.com>
2019-07-05 16:20:13 +0800

    商品管理 页面 init commit

---
+++ src/pages/CommodityManagement.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>CommodityManagement</h1>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'CommodityManagement'
+  }
+</script>
+
+<style scoped>
+
+</style>
+
