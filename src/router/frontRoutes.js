e51d661632277bd4e9752185ea640256a04d3258
XiaoCai20180701 <421586311@qq.com>
2019-07-05 16:29:00 +0800

    前端动态路由表  init commit

---
+++ src/router/frontRoutes.js
@@ -0,0 +1,47 @@
+const frontRoutes = [
+  {
+    name: 'CommodityManagement', // 要跳转的路由名称 不是路径
+    // size: 18, // icon大小
+    type: 'md-home', // icon类型
+    text: '商品管理', // 文本内容,
+    meta: {
+      requireAuth: true
+    }
+  },
+  {
+    text: '订单管理',
+    type: 'ios-paper',
+    meta: {
+      requireAuth: false
+    },
+    children: [
+      {
+        type: 'ios-grid',
+        name: 'NonPayment',
+        text: '未付款'
+      },
+      {
+        text: '未发货',
+        type: 'ios-paper',
+        name: 'Unshipped'
+      },
+      {
+        text: '已发货',
+        type: 'ios-paper',
+        name: 'Shipped'
+      },
+      {
+        text: '售后处理',
+        type: 'ios-paper',
+        name: 'AfterProcessing'
+      },
+      {
+        text: '全部订单',
+        type: 'ios-paper',
+        name: 'AllOrder'
+      }
+    ]
+  }
+]
+
+export default frontRoutes
