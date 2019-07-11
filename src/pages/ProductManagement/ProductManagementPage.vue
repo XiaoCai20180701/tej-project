3466011f2b627ca4fa97fc398e9ca0984abfcceb
XiaoCai20180701 <421586311@qq.com>
2019-07-11 09:26:32 +0800

    商品管理 =》 商品列表页面 init commit

---
+++ src/pages/ProductManagement/ProductManagementPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>
+      ProductManagementPage
+    </h1>
+    <div>
+      <Button type="primary" @click="gotoAddClassification">新增商品分类</Button>
+      <Button type="primary" @click="gotoAddProduct">新增商品</Button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProductManagementPage',
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      gotoAddClassification() {
+        this.$router.push({name: 'AddClassificationPage'})
+      },
+      gotoAddProduct() {
+        this.$router.push({name: 'AddProductPage'})
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
