54842aa1d191dc3a56f3357d383dc59151f84366
XiaoCai20180701 <421586311@qq.com>
2019-07-11 09:23:28 +0800

    商品管理 =》 新增商品分类 =》 商品分类选择 init commit

---
+++ src/pages/ProductManagement/components/addProduct/Classification.vue
@@ -0,0 +1,59 @@
+<template>
+    <Card>
+      <div class="tej-product-box">
+        <p>商品分类选择</p>
+        <Cascader :data="list" change-on-select size="large"></Cascader>
+      </div>
+      <div class="tej-product-box">
+        <p>商品名称</p>
+        <Input v-model="productName" placeholder="请输入商品名称" clearable />
+      </div>
+      <div class="tej-product-box">
+        <p>所属厂商</p>
+        <Select v-model="model1">
+          <OptionGroup v-for="(item, index) in vendorList" :key="index" :label="item.area">
+            <Option v-for="(child, c) in item.children" :value="child.vendorName" :key="child.vendorId">
+              {{ child.vendorName }} ({{child.vendorId}})
+            </Option>
+          </OptionGroup>
+        </Select>
+      </div>
+    </Card>
+</template>
+
+<script>
+  import { getVendorList }  from '@/api/api'
+  export default {
+    name: 'Classification',
+    props: {
+      list: Array,
+    },
+    data() {
+      return {
+        productName: '',
+        vendorList:[],
+        model1: 3,
+//        vendorId: 1
+      }
+    },
+    mounted() {
+      this.getVendorList()
+    },
+    methods: {
+      //获取厂商列表
+      getVendorList() {
+        getVendorList('').then(res => {
+          this.vendorList = res.data.list
+          console.log('vendorList', this.vendorList)
+        })
+          .catch(err => {
+          this.$Message.error('获取厂商列表失败',err)
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
+
