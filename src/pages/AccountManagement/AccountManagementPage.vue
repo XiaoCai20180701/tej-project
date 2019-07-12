1a5a5ee31486adced140e8291453dc68d291f6b1
XiaoCai20180701 <421586311@qq.com>
2019-07-12 17:01:02 +0800

    账户管理页 init commit

---
+++ src/pages/AccountManagement/AccountManagementPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+   <h1>AccountManagementPage</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AccountManagementPage',
+    data() {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
