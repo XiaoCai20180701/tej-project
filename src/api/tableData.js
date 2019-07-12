2bf75610271ff35d9d32496abb5f035670b1dd9e
XiaoCai20180701 <421586311@qq.com>
2019-07-12 16:58:49 +0800

    渲染表格头的数据维护  init commit

---
+++ src/api/tableData.js
@@ -0,0 +1,168 @@
+/**
+ * 商品管理
+ * */
+export const productManagementTable = [
+  {
+    'title': '编号'
+  },
+  {
+    'title': '商品类别'
+  },
+  {
+    'title': '商品名称'
+  },
+  {
+    'title': '零售价'
+  },
+  {
+    'title': '库存'
+  },
+  {
+    'title': '访问量'
+  },
+  {
+    'title': '销量'
+  },
+  {
+    'title': '厂商'
+  },
+  {
+    title: '操作',
+    slot: 'action',
+  }
+]
+
+/**
+* 零售商管理 => 已审核列表
+* */
+export const checkedRetailTable = [
+  {
+    'title': '编号'
+  },
+  {
+    'title': '商家名称'
+  },
+  {
+    'title': '所在省市'
+  },
+  {
+    'title': '联系人'
+  },
+  {
+    'title': '联系号码'
+  },
+  {
+    'title': '成交订单数'
+  },
+  {
+    'title': '总成交额'
+  },
+  {
+    title: '操作',
+    slot: 'action',
+  }
+]
+
+/**
+ * 零售商管理 => 待审核列表
+ * */
+export const unCheckedRetailTable = [
+  {
+    'title': '商家名称'
+  },
+  {
+    'title': '所在省市'
+  },
+  {
+    'title': '联系人'
+  },
+  {
+    'title': '联系号码'
+  },
+  {
+    'title': '经营许可证'
+  },
+  {
+    'title': '生产环境'
+  },
+  {
+    'title': '店铺商标'
+  },
+  {
+    title: '操作',
+    slot: 'action',
+  }
+]
+
+/**
+ * 数据管理 => 全站销量前十商品排行榜
+ * */
+export const  productSaleTable = [
+  {
+    'title': '编号'
+  },
+  {
+    'title': '商品名称'
+  },
+  {
+    'title': '厂商'
+  },
+  {
+    'title': '销量(件)'
+  },
+]
+
+/**
+ * 数据管理 => 全站访问量前十商品排行榜
+ * */
+export const  productAccessTable = [
+  {
+    'title': '编号'
+  },
+  {
+    'title': '商品名称'
+  },
+  {
+    'title': '厂商'
+  },
+  {
+    'title': '访问量(次)'
+  },
+]
+
+/**
+ * 数据管理 => 全站销量前十厂商
+ * */
+export const  vendorSaleTable = [
+  {
+    'title': '编号'
+  },
+  {
+    'title': '厂商'
+  },
+  {
+    'title': '地区'
+  },
+  {
+    'title': '销量(件)'
+  },
+]
+
+/**
+ * 数据管理 => 全站前十厂商、商家合作次数
+ * */
+export const  cooperationTable = [
+  {
+    'title': '编号'
+  },
+  {
+    'title': '厂商'
+  },
+  {
+    'title': '零售商'
+  },
+  {
+    'title': '合作次数'
+  },
+]
+
