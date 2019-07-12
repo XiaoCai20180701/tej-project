4b1c3b0781c9128401c892962f0cc9215d49bd59
XiaoCai20180701 <421586311@qq.com>
2019-07-12 17:04:41 +0800

    零售商管理 => 待审核页 init commit

---
+++ src/pages/VendorManagement/UnCheckedVendorPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="tej-main">
+    <h1>UnCheckedVendorPage</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'UnCheckedVendorPage',
+  }
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
+
