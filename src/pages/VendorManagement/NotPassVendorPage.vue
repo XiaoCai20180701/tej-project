721dd6039af0c515c5ac336fcc33595af8b9d63f
XiaoCai20180701 <421586311@qq.com>
2019-07-12 17:04:28 +0800

    厂商管理 => 未通过页 init commit

---
+++ src/pages/VendorManagement/NotPassVendorPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="tej-main">
+    <h1>NotPassVendorPage</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NotPassVendorPage',
+  }
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
