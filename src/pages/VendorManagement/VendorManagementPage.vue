1a745a1ef1ef436e78e12f981f81ea38b40632d7
XiaoCai20180701 <421586311@qq.com>
2019-07-12 17:05:04 +0800

    厂商管理 => 全部厂商页 init commit

---
+++ src/pages/VendorManagement/VendorManagementPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="tej-main">
+    <h1>VendorManagementPage</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'VendorManagementPage',
+  }
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
+
