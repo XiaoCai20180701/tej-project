b98d7dc0a3e6d83f872376d0aeeec0bdb08f699f
XiaoCai20180701 <421586311@qq.com>
2019-07-12 17:04:08 +0800

    厂商管理 => 已审核页 init commit

---
+++ src/pages/VendorManagement/CheckedVendorPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tej-main">
+    <h1>CheckedVendorPage</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CheckedVendorPage',
+  }
+</script>
+
+<style scoped>
+
+</style>
+
+
+
