dfd38a3338356243c705ee56f9fc16184dc139f6
XiaoCai20180701 <421586311@qq.com>
2019-07-12 17:03:36 +0800

    零售商管理 => 全部零售商页 init commit

---
+++ src/pages/RetailManagement/RetailManagementPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>RetailManagementPage</h1>
+
+</template>
+
+<script>
+  import  TejTable  from '@/components/TejTable'
+  export default {
+    name: 'RetailManagementPage',
+    commponents: {
+      'TejTable': TejTable
+    },
+    data() {
+      return {
+
+      }
+    },
+    mounted(){},
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+
