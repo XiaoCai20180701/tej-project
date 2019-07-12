a18bc4bb533ca384731e5ad92f410d3dd27d86ce
XiaoCai20180701 <421586311@qq.com>
2019-07-12 17:02:55 +0800

    零售商管理 => 已审核页 init commit

---
+++ src/pages/RetailManagement/CheckedPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tej-main">
+    <TejTable></TejTable>
+  </div>
+</template>
+
+<script>
+  import  TejTable  from '@/components/TejTable'
+  export default {
+    name: 'CheckedPage',
+    components: {
+      'TejTable': TejTable
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+
+
+
