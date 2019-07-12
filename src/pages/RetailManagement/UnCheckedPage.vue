fe3060940c0b1fdde9349c964b647e08bf713c89
XiaoCai20180701 <421586311@qq.com>
2019-07-12 17:03:51 +0800

    零售商管理 => 待审核页 init commit

---
+++ src/pages/RetailManagement/UnCheckedPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tej-main">
+    <TejTable></TejTable>
+  </div>
+</template>
+
+<script>
+  import  TejTable  from '@/components/TejTable'
+  export default {
+    name: 'UnCheckedPage',
+    components: {
+      'TejTable': TejTable
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+
