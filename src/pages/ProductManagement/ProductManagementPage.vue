254b619da8d7bebaaf6d64e6e62bf07009ff9ecb
XiaoCai20180701 <421586311@qq.com>
2019-07-12 17:02:14 +0800

    添加 table业务组件

--- src/pages/ProductManagement/ProductManagementPage.vue
+++ src/pages/ProductManagement/ProductManagementPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="tej-main">
     <h1>
       ProductManagementPage
     </h1>
@@ -7,18 +7,33 @@
       <Button type="primary" @click="gotoAddClassification">新增商品分类</Button>
       <Button type="primary" @click="gotoAddProduct">新增商品</Button>
     </div>
+    <TejTable></TejTable>
   </div>
 </template>
 
 <script>
+  import {getProductList} from '@/api/api'
+  import  TejTable  from '@/components/TejTable'
   export default {
     name: 'ProductManagementPage',
+    components: {
+      'TejTable': TejTable
+    },
     data() {
       return {
 
       }
     },
+    mounted(){
+      this.getProductList()
+    },
     methods: {
+      getProductList(){
+        getProductList({page:1, pageSize: 10}).then(res => {
+          console.log('res',res)
+        })
+          .catch(err => {console.log('err', err)})
+      },
       gotoAddClassification() {
         this.$router.push({name: 'AddClassificationPage'})
       },
