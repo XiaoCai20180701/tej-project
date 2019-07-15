85103d24681bd57b38c8c6298f8f6c80d5c405cf
XiaoCai20180701 <421586311@qq.com>
2019-07-15 18:01:10 +0800

    实现 实时新增/删除一级目录、二级目录 功能

--- src/pages/ProductManagement/AddClassificationPage.vue
+++ src/pages/ProductManagement/AddClassificationPage.vue
@@ -1,107 +1,161 @@
 <template>
-    <Row style="background:#eee;">
-      <Col span="4">
+  <Row style="background:#eee;">
+    <Col span="4">
+    <Card class="tej-card-tree">
+      <p slot="title">已有分类</p>
+      <Tree :data="list"></Tree>
+    </Card>
+    </Col>
+    <Col span="14" offset="2">
+    <Card>
+      <p slot="title">新建分类</p>
+      <Row>
+        <Col span="8">
         <Card>
-          <p slot="title">已有分类</p>
-          <Tree :data="list"></Tree>
+          <p slot="title">一级目录</p>
+          <RadioGroup v-model="checkParent" @on-change="parentChange">
+            <div v-for="(item, index) in parentList" :key="index">
+              <Radio :label="item.id" @click="">{{item.title}}</Radio>
+              <b class="tej-delete-txt" @click="deleteFun(item)">删除</b>
+            </div>
+            <div>
+              <Radio>
+                <slot>
+                  <Input v-model="newValue" clearable placeholder="请输入新分类" style="width: 160px"
+                         @on-change="getInputValue"/>
+                </slot>
+              </Radio>
+              <b class="tej-add-txt" @click="addParent">新增</b>
+            </div>
+          </RadioGroup>
         </Card>
-      </Col>
-      <Col span="14" offset="2">
+        </Col>
+        <Col span="8" offset="2">
         <Card>
-          <p slot="title">新建分类</p>
-          <Row>
-            <Col span="8">
-              <Card>
-                <p slot="title">一级目录</p>
-                <CheckboxGroup v-model="checkParentGroup">
-                  <div v-for="(item, index) in parentList" :key="index">
-                    <Checkbox :label="item.title"></Checkbox><b class="tej-delete-txt">删除</b>
-                  </div>
-                  <div>
-                    <Checkbox>
-                      <slot>
-                        <Input v-model="newValue" clearable placeholder="请输入新分类" style="width: 160px" @on-change="getInputValue"/>
-                      </slot>
-                    </Checkbox>
-                    <b class="tej-add-txt" @click="add">新增</b>
-                  </div>
-                </CheckboxGroup>
-              </Card>
-            </Col>
-            <Col span="8" offset="2">
-              <Card>
-                <p slot="title">二级目录</p>
-                <CheckboxGroup v-model="checkChildrenGroup">
-                  <div v-for="(child, c) in childrenList" :key="c">
-                    <Checkbox :label="child.title"></Checkbox><b class="tej-delete-txt">删除</b>
-                  </div>
-                </CheckboxGroup>
-              </Card>
-            </Col>
-          </Row>
-          <Row style="margin-top: 20px">
-            <Col span="5" offset="10">
-              <Button>取消</Button>
-              <Button type="primary" @click="sumbit">确定</Button>
-            </Col>
-          </Row>
+          <p slot="title">二级目录</p>
+          <RadioGroup v-model="checkChildren">
+            <div v-for="(child, c) in childrenList" :key="c">
+              <Radio :label="child.title"></Radio>
+              <b class="tej-delete-txt" @click="deleteFun(child)">删除</b>
+            </div>
+            <div>
+              <Radio>
+                <slot>
+                  <Input v-model="newChildrenValue" clearable placeholder="请输入新分类" style="width: 160px"
+                         @on-change="getInputChildrenValue"/>
+                </slot>
+              </Radio>
+              <b class="tej-add-txt" @click="addChildren">新增</b>
+            </div>
+          </RadioGroup>
         </Card>
-      </Col>
-    </Row>
+        </Col>
+      </Row>
+    </Card>
+    </Col>
+  </Row>
 </template>
 
 <script>
-  import { getClassificationlist, editClassification } from '@/api/api'
+  import {getClassificationlist, editClassification, addParentType, deleteType} from '@/api/api'
+
   export default {
     name: 'AddClassificationPage',
     data() {
       return {
         list: [],
-        parentList:[],
-        checkParentGroup:[],
-        childrenList:[],
-        checkChildrenGroup: [],
-        newValue: ''
+        parentList: [],
+        childrenList: [],
+        newValue: '',
+        newChildrenValue: '',
+        checkParent: 1,
+        checkChildren: 9
       }
     },
     mounted() {
       this.getClassificationlist()
     },
     methods: {
-      //提交
-      sumbit(){
-        let data = {"data": []}
-        let params =
-        editClassification(params).then(res => {
-          this.$Message.success('成功新增商品分类')
-        })
-          .catch(err => {
-            this.$Message.error('新增商品分类失败', err)
+      parentChange(e) {
+        this.parentList.find((item) => {
+          this.childrenList = item.children
+          return item.id == e
         })
       },
-      getInputValue(event){
+      getInputValue(event) {
         this.newValue = event.target.value
       },
-      //新增一级目录
-      add(){
-        this.parentList.push({title: this.newValue})
-        this.newValue = ''
+      getInputChildrenValue(event) {
+        this.newChildrenValue = event.target.value
+      },
+      //删除目录
+      deleteFun(item){
+        let content = '当您在删除「' + item.title + '」时，将同时删除「' + item.title + '」中的所有二级目录，请您确认是否执行此操作'
+        //删除一级目录设置提示
+        if(item.children){
+          this.$Modal.confirm({
+            title: '删除目录',
+            content: content,
+            onOk: () => {
+              this.deleteTypeFun(item.id)
+              this.$Message.success('删除成功')
+            },
+            onCancel: () => {
+              this.$Message.success('取消删除')
+            }
+          })
+        }else {
+          //直接删除二级目录
+          this.$Message.success('删除成功')
+        }
+      },
+      deleteTypeFun(id){
+        deleteType(id).catch(err => {
+          this.$Message.error('删除目录失败',err)
+        })
+      },
+      //新增目录
+      addParent(){
+        this.isParent = true
+        this.add(this.isParent)
+      },
+      addChildren(){
+        this.isParent = false
+        this.add(this.isParent)
+      },
+      add(isParent) {
+        addParentType({
+          typeParentName: this.newValue,
+          typeParentId: isParent ? '': 3
+        })
+          .then(res => {
+            console.log('新增目录', res)
+            //  this.parentList.push({title: this.newValue})
+            if(isParent){
+              this.list.push({title: this.newValue})
+              this.newValue = ''
+            }else {
+              this.childrenList.push({title: this.newChildrenValue})
+              this.newChildrenValue = ''
+            }
+          })
+          .catch(err => {
+            this.$Message.error('新增失败', err)
+          })
       },
       //获取已有分类、一级目录、二级目录
-       getClassificationlist() {
+      getClassificationlist() {
         getClassificationlist('').then(res => {
-          this.list = res.data.list
-          this.list.map(item => {
-            this.parentList.push({id: item.id,title: item.title})
-            this.checkParentGroup.push(item.id)
-            item.children.map(child => {
-              this.childrenList.push({id: child.id, title: child.title})
-              this.checkChildrenGroup.push(child.id)
-            })
+          let data = res.data
+          this.list = data.list
+          this.parentList = data.list
+          data.list.find((item, index) => {
+            this.childrenList = item.children
+            this.checkParent = item.id
+            return index == 0
           })
-          console.log('parentList',this.parentList)
         }).catch(err => {
-          this.$Message.error('获取商品分类失败！',err)
+          this.$Message.error('获取商品分类失败！', err)
         })
       }
     }
@@ -110,9 +164,17 @@
 
 <style scoped>
   .ivu-card-head {
-    background: rgb(250,250,250) !important;
+    background: rgb(250, 250, 250) !important;
+  }
+  .tej-card-tree .ivu-card-body{
+    min-height: 79vh;
   }
   .ivu-card-body {
-    min-height: 75vh;
+    min-height: 70vh;
   }
+
+  .ivu-radio-inner, .ivu-radio-inner:after {
+    border-radius: 0 !important;
+  }
+
 </style>
