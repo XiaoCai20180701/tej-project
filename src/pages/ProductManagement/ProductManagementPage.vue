601ec4836706f61e7c243f80458033181038f1ef
XiaoCai20180701 <421586311@qq.com>
2019-07-15 18:02:37 +0800

    完善TejTable表格组件的调用

--- src/pages/ProductManagement/ProductManagementPage.vue
+++ src/pages/ProductManagement/ProductManagementPage.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="tej-main">
-    <h1>
-      ProductManagementPage
-    </h1>
-    <div>
-      <Button type="primary" @click="gotoAddClassification">新增商品分类</Button>
-      <Button type="primary" @click="gotoAddProduct">新增商品</Button>
-    </div>
-    <TejTable></TejTable>
+    <TejTable
+      :columns-data="columnsData"
+      :table-data="tableData"
+      :page="page"
+      @page-change-callback="pageChange"
+      @pageSize-change-callback="pageSizeChange"
+      @area-change-callback="areaChange"
+      @keywords-change-callback="keywordsChange"
+    >
+      <div slot="btn">
+        <Button type="primary" @click="gotoAddClassification">新增商品分类</Button>
+        <Button type="primary" @click="gotoAddProduct">新增商品</Button>
+      </div>
+    </TejTable>
   </div>
 </template>
 
 <script>
   import {getProductList} from '@/api/api'
-  import  TejTable  from '@/components/TejTable'
+  import {productManagementTable} from '@/api/tableData'
+  import TejTable from '@/components/TejTable'
+
   export default {
     name: 'ProductManagementPage',
     components: {
@@ -21,18 +29,64 @@
     },
     data() {
       return {
-
+        columnsData: [],
+        tableData: [],
+        page: {
+          index: 1,
+          size: 10,
+          total: 10
+        },
+        regionGroupID: 1,
+        keywords: ''
       }
     },
-    mounted(){
-      this.getProductList()
+    mounted() {
+      this.columnsData = productManagementTable
+      this.getList()
     },
     methods: {
-      getProductList(){
-        getProductList({page:1, pageSize: 10}).then(res => {
-          console.log('res',res)
+      keywordsChange(keywords) {
+        console.log('keywords 回调', keywords)
+        this.keywords = keywords
+        this.getList()
+      },
+      areaChange(areaId) {
+        console.log('area 回调', areaId)
+        this.regionGroupID = areaId
+        this.getList()
+      },
+      pageChange(page) {
+        this.page = page
+        this.getList()
+      },
+      pageSizeChange(page) {
+        this.page = page
+        this.getList()
+      },
+      getList() {
+        getProductList({
+          page: this.page.index,
+          pageSize: this.page.size,
+          area: this.regionGroupID,
+          keywords: this.keywords
+        }).then(res => {
+          let data = res.data
+          this.tableData = data.list
+          this.page = {
+            index: data.page,
+            size: data.pageSize,
+            total: data.total
+          }
+          data.list.map((item) => {
+//            console.log('key Object---------',Object.keys(item))
+            this.columnsData.map((col, c) => {
+              col['key'] = Object.keys(item)[c]
+            })
+          })
         })
-          .catch(err => {console.log('err', err)})
+          .catch(err => {
+            this.$Message.error('获取商品列表失败', err)
+          })
       },
       gotoAddClassification() {
         this.$router.push({name: 'AddClassificationPage'})
