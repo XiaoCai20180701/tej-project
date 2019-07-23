386a077fab7b10f6e6d29b9dc476c94582e6ee88
XiaoCai20180701 <421586311@qq.com>
2019-07-23 19:28:06 +0800

    订单item 组件  init commit

---
+++ src/components/OrderItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="tej-order-item">
+    <Card>
+      <div slot="title" class="tej-order-title">
+        <div class="item"><p>已发货：</p><p>2019年6月26日</p></div>
+        <div class="item"><p>订单编号：</p><p>2019年6月26日</p></div>
+        <div class="item"><p>发货至：</p><p>2019年6月26日</p></div>
+        <div class="item"><p>送货至：</p><p>2019年6月26日</p></div>
+        <div class="item"><a>查看订单详情</a></div>
+      </div>
+      <div>dddd</div>
+    </Card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'OrderItem',
+    props: {
+      data: {
+        type: Object
+      }
+    },
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style>
+  .tej-order-item .ivu-card-head{
+    background: rgb(243,247,250);
+  }
+  .tej-order-title {
+    display: flex;
+  }
+  .tej-order-title p{
+    font-weight: normal;
+  }
+  .tej-order-title .item {
+    flex: 1;
+  }
+  .tej-order-title .item:last-child {
+    flex: 2;
+    line-height: 44px;
+    text-align: right;
+  }
+</style>
+
+
+
