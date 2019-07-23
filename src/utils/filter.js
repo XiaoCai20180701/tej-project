8f6373ab13336cc38a67c2526f6cd587f751e045
XiaoCai20180701 <421586311@qq.com>
2019-07-23 19:21:47 +0800

    格式化金额，保留两位小数

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -4,6 +4,10 @@ const filter = {
     //  return (val).toFixed(2).replace(/(\d)(?=(\d{3}))/g, '$1,')
       return (val.toString().indexOf ('.') !== -1) ? val.toLocaleString() : val.toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,')
     }
+  },
+  'money': function (val) {
+    let value = Number(val)
+    return value.toFixed(2)
   }
 }
 export default filter
