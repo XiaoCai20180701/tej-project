1ace5c4e33d9c0a97321f131105399499388846a
XiaoCai20180701 <421586311@qq.com>
2019-07-24 15:25:01 +0800

    厂商管理 待审核页面 init commit

--- src/pages/VendorManagement/UnCheckedVendorPage.vue
+++ src/pages/VendorManagement/UnCheckedVendorPage.vue
@@ -1,20 +1,98 @@
 <template>
   <div class="tej-main">
-    <h1>UnCheckedVendorPage</h1>
+    <TejTable
+      :columns-data="columnsData"
+      :table-data="tableData"
+      :page="page"
+      input-text="请输入厂商编号/名称/联系人"
+      @page-change-callback="pageChange"
+      @pageSize-change-callback="pageSizeChange"
+      @area-change-callback="areaChange"
+      @keywords-change-callback="keywordsChange"
+    >
+      <div slot="age"></div>
+    </TejTable>
   </div>
 </template>
 
 <script>
+  import  TejTable  from '@/components/TejTable'
+  import { postVendorList} from '@/api/api'
+  import { unCheckedVendorTable, auditType } from '@/api/tableData'
   export default {
-    name: 'UnCheckedVendorPage'
+    name: 'UnCheckedVendorPage',
+    components: {
+      'TejTable': TejTable
+    },
+    data(){
+      return {
+        columnsData: [],
+        tableData: [],
+        page: {
+          index: 1,
+          size: 10,
+          total: 10
+        },
+        keywords: '',
+        blockId: null,
+      }
+    },
+    mounted(){
+      this.columnsData = unCheckedVendorTable
+      this.getList()
+    },
+    methods: {
+      keywordsChange(keywords) {
+        console.log('keywords 回调', keywords)
+        this.keywords = keywords
+        this.getList()
+      },
+      areaChange(area){
+        console.log('area 回调',area)
+        this.blockId = area
+        this.getList()
+      },
+      pageChange(page){
+        this.page = page
+        this.getList()
+      },
+      pageSizeChange(page){
+        this.page = page
+        this.getList()
+      },
+      getList(){
+        postVendorList({
+          page: this.page.index,
+          pageSize: this.page.size,
+          keywords: this.keywords,
+          blockId: this.blockId,
+          status: auditType.unChecked
+        }).then(res => {
+          let data = res.data
+          this.tableData = data.vendorList
+          this.page = {
+            index: data.page,
+            size: data.pageSize,
+            total: data.total
+          }
+          data.vendorList.map((item) => {
+            console.log('key Object---------',Object.keys(item))
+            this.columnsData.map((col,c) => {
+              col['key'] = Object.keys(item)[c + 1]
+            })
+          })
+        })
+          .catch(err => {
+            this.$Message.error('获取厂商待审核列表失败',err)
+          })
+      },
+    }
   }
 </script>
 
 <style scoped>
 
 </style>
-
-
 
 
 
