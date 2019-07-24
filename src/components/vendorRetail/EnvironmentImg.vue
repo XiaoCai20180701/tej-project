c738ade1388d8ca02db20ce1443488225add7c24
XiaoCai20180701 <421586311@qq.com>
2019-07-24 15:22:39 +0800

    厂商、零售商详情 图片轮播图组件 init commit

---
+++ src/components/vendorRetail/EnvironmentImg.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="environment-imglist">图片轮播
+    <!--<Carousel-->
+    <!--v-model="environmentImgIndex"-->
+    <!--autoplay-->
+    <!--:height="148"-->
+    <!--&gt;-->
+    <!--<CarouselItem v-for="(item, index) in vendorDetail.vendorEnvironmentImg" :key="index">-->
+    <!--<img src="http://dummyimage.com/200x100/4A7BF7&text=Hello"/>-->
+    <!--</CarouselItem>-->
+    <!--</Carousel>-->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'EnvironmentImg',
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
