f20a72ea3947db733c10d34c25acf0dc1510479b
XiaoCai20180701 <421586311@qq.com>
2019-07-24 18:16:57 +0800

    添加prod环境

--- config/proxyConfig.js
+++ config/proxyConfig.js
@@ -1,5 +1,13 @@
 module.exports = {
   proxy: {
+    '/prod': {
+      target: 'http://47.92.209.177:8080/tej/rest',
+      secure: false,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/prod':'/'
+      }
+    },
     '/api': {
       target: 'https://www.easy-mock.com/mock/5d119aa1e8830023398fe939',
       secure: false,
@@ -9,11 +17,19 @@ module.exports = {
       }
     },
     '/yz': {
-      target: 'http://192.168.1.101:8080/tej/rest',
+      target: 'http://192.168.1.102:8080/tej/rest',
       secure: false,
       changeOrigin: true,
       pathRewrite: {
         '^/yz':'/'
+      }
+    },
+    '/wd': {
+      target: 'http://192.168.1.104:8080/tej/rest',
+      secure: false,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/wd':'/'
       }
     }
   }
