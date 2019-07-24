563c69e029c4ef8de984cad64587cddaf4a0eabd
XiaoCai20180701 <421586311@qq.com>
2019-07-24 10:45:43 +0800

    未发货 完成搜索区、表格、分页

--- src/pages/OrderManagement/Unshipped.vue
+++ src/pages/OrderManagement/Unshipped.vue
@@ -1,20 +1,126 @@
 <template>
   <div>
-    <h1>Unshipped</h1>
+    <OrderSearchForm @vendor-callback="vendorCallback"
+                     @retail-callback="retailCallback"
+                     @ok-callback="searchCallback"
+    ></OrderSearchForm>
+    <OrderTable :order-columns-table="orderColumns"
+                :order-data-table="orderData"
+                :page="page"
+                @page-change-callback="pageChangeCallback"
+                @pageSize-change-callback="pageSizeChangeCallback"
+    ></OrderTable>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Unshipped',
-  data(){
-    return {
+  import { postOrderList} from '@/api/api'
+  import { orderType } from '@/api/tableData'
+  import OrderSearchForm from './components/OrderSearchForm'
+  import OrderTable from './components/OrderTable'
+  import expandRow from './components/OrderItemExpand'
 
+  export default {
+    name: 'Unshipped',
+    components: { expandRow,OrderSearchForm,OrderTable },
+    data() {
+      return {
+        page: {
+          index: 1,
+          size: 10,
+          total: 10
+        },
+        orderList: [],
+        orderColumns: [
+          {
+            type: 'expand',
+            width: 50,
+            render: (h, params) => {
+              return h(expandRow, {
+                props: {
+                  row: params.row
+                }
+              })
+            }
+          },
+          {
+            title: '订单编号',
+            key: 'orderId'
+          },
+          {
+            title: '状态',
+            key: 'orderType'
+          },
+          {
+            title: '发货时间',
+            key: 'orderSendTime'
+          },
+          {
+            title: '发货至',
+            key: 'vendorName'
+          },
+          {
+            title: '送货至',
+            key: 'retailName'
+          },
+          {
+            title: '操作',
+            slot: 'action'
+          }
+        ],
+        orderData: []
+      }
+    },
+    mounted() {
+      this.getList()
+    },
+    methods: {
+      pageChangeCallback(data){
+        console.log('page callback',data)
+        this.getList()
+      },
+      pageSizeChangeCallback(data){
+        console.log('pageSize callback',data)
+        this.getList()
+      },
+      searchCallback(data){
+        console.log('确认 回调',data)
+        this.getList()
+      },
+      vendorCallback(data){
+        console.log('厂商历史记录点击 回调',data)
+        this.getList()
+      },
+      retailCallback(data){
+        console.log('零售商历史记录点击 回调',data)
+        this.getList()
+      },
+      getList() {
+        let params = {
+          page: this.page.index,
+          pageSize: this.page.size,
+          orderType: orderType.unshipped,
+          vendorName: this.vendorName,
+          retailName: this.retailName,
+          startTime: this.startTime,
+          endTime: this.endTime
+        }
+        postOrderList(params).then(res => {
+          console.log('未发货订单列表', res.data)
+          let data = res.data
+          this.orderData = data.orderList
+          this.page = {
+            index: data.page,
+            size: data.pageSize,
+            total: data.total
+          }
+        }).catch(err => {
+          this.$Message.error('获取未发货订单列表失败', err)
+        })
+      }
     }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 </style>
