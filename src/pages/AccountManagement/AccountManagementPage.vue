f1b82498be2e4dca921614cfc74bf05834492e3d
XiaoCai20180701 <421586311@qq.com>
2019-07-25 17:46:51 +0800

    完成账户列表页

--- src/pages/AccountManagement/AccountManagementPage.vue
+++ src/pages/AccountManagement/AccountManagementPage.vue
@@ -1,19 +1,141 @@
 <template>
   <div>
-   <h1>AccountManagementPage</h1>
+    <TejTable
+      :columns-data="columnsData"
+      :table-data="tableData"
+      :page="page"
+      @page-change-callback="pageChange"
+      @pageSize-change-callback="pageSizeChange"
+    >
+      <div slot="area"></div>
+      <div slot="extra">
+        <RadioGroup v-model="checkedRole" @on-change="roleChange">
+          <Radio v-for="(item, index) in roleList"
+                 :key="index"
+                 :label="item.id"
+                 class="tej-radio"
+          >{{item.name}}
+          </Radio>
+        </RadioGroup>
+      </div>
+      <div slot="search" class="notxt">1</div>
+      <div slot="btn">
+          <Button type="primary" class="tej-addrole-btn">添加账户</Button>
+      </div>
+      <a slot="action-btn"
+         slot-scope="props"
+         class="tej-disable-txt"
+         @click="disableUser(props.row.userId)"
+      >禁用{{props.row.userId}}</a>
+    </TejTable>
   </div>
 </template>
 
 <script>
+  import TejTable from '@/components/TejTable'
+  import  { accountTable, roleType, userStatusType } from '@/api/tableData'
+  import { postUserList, getRolelist, putEditUserStatus } from '@/api/api'
   export default {
     name: 'AccountManagementPage',
+    components: {
+      'TejTable': TejTable
+    },
     data() {
       return {
+        columnsData: [],
+        tableData: [],
+        page: {
+          index: 1,
+          size: 10,
+          total: 10
+        },
+        roleId: roleType.admin,
+        checkedRole: 0,
+        roleList: [],
+        userStatus: userStatusType.enable
+      }
+    },
+    mounted(){
+      this.columnsData = accountTable
+      this.getRolelistFun()
+      this.getList()
+    },
+    methods: {
+      disableUser(userId){
+        console.log('props.row.userId',userId)
+        this.editUserStatus(userId)
+      },
+      roleChange(e){
+        console.log('角色change', e)
+        this.getList()
+      },
+      pageChange(page) {
+        this.page = page
+        this.getList()
+      },
+      pageSizeChange(page) {
+        this.page = page
+        this.getList()
+      },
+      getList(){
+        let params = {
+          page: this.page.index,
+          pageSize: this.page.size,
+          roleId: this.roleId
+        }
+        postUserList(params).then(res=> {
+          console.log('账户列表',res)
+          let data = res.data
+          this.tableData = data.list
+          data.list.map((item) => {
+//            console.log('key Object---------',Object.keys(item))
+            this.columnsData.map((col, c) => {
+              col['key'] = Object.keys(item)[c]
+            })
+          })
+        }).catch(err => {
+          this.$Message.error({
+            content: '获取账户列表失败'
+          })
+        })
+      },
+      getRolelistFun(){
+        getRolelist().then(res => {
+          console.log('角色列表', res.data)
+          this.roleList = res.data.list
+        }).catch(err => {
+          this.$Message.error({
+            content: '获取角色列表失败'
+          })
+        })
+      },
+      editUserStatus(userId){
+        let params = {
+          userId: userId,
+          isUsed: this.userStatus
+        }
+        putEditUserStatus(params).then(res => {
+          this.$Message.success({
+            content: '成功修改状态'
+          })
+        }).catch(err => {
+          this.$Message.error({
+            content: '修改账户状态失败'
+          })
+        })
       }
     }
   }
 </script>
 
 <style scoped>
-
+.tej-addrole-btn {
+  margin-bottom: 20px;
+}
+  .notxt {
+    color: transparent;
+  }
+  .tej-disable-txt {
+    color: red;
+  }
 </style>
