ba7673fdaebcea83ecec01a017c96f1e433d4a31
XiaoCai20180701 <421586311@qq.com>
2019-07-26 17:14:39 +0800

    厂商未通过列表 init commit

--- src/pages/VendorManagement/NotPassVendorPage.vue
+++ src/pages/VendorManagement/NotPassVendorPage.vue
@@ -1,19 +1,95 @@
 <template>
   <div class="tej-main">
-    <h1>NotPassVendorPage</h1>
+    <TejTable
+      :columns-data="columnsData"
+      :table-data="tableData"
+      :page="page"
+      input-text="请输入厂商编号/名称/联系人"
+      @page-change-callback="pageChange"
+      @pageSize-change-callback="pageSizeChange"
+      @area-change-callback="areaChange"
+      @keywords-change-callback="keywordsChange"
+    >
+      <div slot="age"></div>
+    </TejTable>
   </div>
 </template>
 
 <script>
+  import { postVendorList } from '@/api/api'
+  import { notPassVendorTable, auditType } from '@/api/tableData'
+  import  TejTable  from '@/components/TejTable'
   export default {
     name: 'NotPassVendorPage',
+    components: {
+      'TejTable': TejTable
+    },
+    data() {
+      return {
+        columnsData: [],
+        tableData: [],
+        page: {
+          index: 1,
+          size: 10,
+          total: 10
+        },
+        keywords: '',
+        blockId: null,
+      }
+    },
+    mounted(){
+      this.columnsData = notPassVendorTable
+      this.getList()
+    },
+    methods: {
+      keywordsChange(keywords) {
+        console.log('keywords 回调', keywords)
+        this.keywords = keywords
+        this.getList()
+      },
+      areaChange(area){
+        console.log('area 回调',area)
+        this.blockId = area
+        this.getList()
+      },
+      pageChange(page){
+        this.page = page
+        this.getList()
+      },
+      pageSizeChange(page){
+        this.page = page
+        this.getList()
+      },
+      getList(){
+        postVendorList({
+          page: this.page.index,
+          pageSize: this.page.size,
+          keywords: this.keywords,
+          blockId: this.blockId,
+          vendorType: auditType.notPass
+        }).then(res => {
+          let data = res.data
+          this.tableData = data.vendorList
+          this.page = {
+            index: data.page,
+            size: data.pageSize,
+            total: data.total
+          }
+          data.vendorList.map((item) => {
+            this.columnsData.map((col,c) => {
+              col['key'] = Object.keys(item)[c+1]
+            })
+          })
+        })
+          .catch(err => {
+            this.$Message.error('获取厂商未通过列表失败',err)
+          })
+      },
+    }
   }
 </script>
 
 <style scoped>
 
 </style>
-
-
-
 
