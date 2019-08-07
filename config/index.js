7a6fb93a7b324c45fe328334aa17f3957e7ce154
XiaoCai20180701 <421586311@qq.com>
2019-08-07 18:13:37 +0800

    修改host地址

--- config/index.js
+++ config/index.js
@@ -14,7 +14,8 @@ module.exports = {
     proxyTable: proxyConfig.proxy,
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+   // host: 'localhost', // can be overwritten by process.env.HOST
+    host: '192.168.1.108',
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
