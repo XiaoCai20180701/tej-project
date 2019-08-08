8d4123a7ef44357c895b2a55185dcb2e084fe5a0
XiaoCai20180701 <421586311@qq.com>
2019-08-08 10:31:45 +0800

    修改autoOpenBrowser 值为 true

--- config/index.js
+++ config/index.js
@@ -14,10 +14,10 @@ module.exports = {
     proxyTable: proxyConfig.proxy,
 
     // Various Dev Server settings
-   // host: 'localhost', // can be overwritten by process.env.HOST
-    host: '192.168.1.108',
+   host: 'localhost', // can be overwritten by process.env.HOST
+   //  host: '192.168.1.108',
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
@@ -51,7 +51,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: './static',
+    assetsSubDirectory: './static/',
     assetsPublicPath: './',
 
     /**
