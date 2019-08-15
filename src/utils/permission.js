4f3ff534ea3a8c1aa8fd79b293fed7ea6b22f264
XiaoCai20180701 <421586311@qq.com>
2019-08-15 16:24:34 +0800

    全局路由钩子函数

--- src/utils/permission.js
+++ src/utils/permission.js
@@ -1,8 +1,35 @@
 import router from '@/router'
 import store from '@/store'
-import { menusToRoutes } from '@/utils'
-import { getRes } from '@/api/api'
-import  frontRoutes  from '@/router/frontRoutes'
+import {menusToRoutes} from '@/utils'
+import {getRes} from '@/api/api'
+import frontRoutes from '@/router/frontRoutes'
+
+//全局钩子函数
+//中间件  在请求和响应中间写几个处理的程序，每一个程序处理完成后，交给下一个中间件处理
+//1.判断是否需要登录
+// router.beforeEach((to, from, next) => {
+//   console.log('进入每一个路由前！！！')
+//   console.log('路由 to', to)  //到哪里去
+//   console.log('路由 from', from) //从哪里来
+//   //1.判断是否需要登录
+//   if (to.meta.requireAuth) {
+//     //需要登录
+//     //2.判断是否登录，localStorage里有没有token
+//     let checked = localStorage.getItem('token')
+//     if (!checked) {  //没有登录,则需要登录
+//       next('/login')
+//     } else {
+//       //动态生成路由
+//       const routes = menusToRoutes(frontRoutes)
+//       router.addRoutes(routes)
+//       next({path: to.path || '/'})
+//     }
+//   } else {
+//     next()
+//   }
+//
+// })
+
 
 // 是否有菜单数据
 let hasMenus = false
@@ -18,7 +45,7 @@ router.beforeEach(async (to, from, next) => {
             } else {
                 try {
                     // 这里可以用 await 配合请求后台数据来生成路由
-                   // const routes = menusToRoutes(store.state.menuItems)
+                //    const routes = menusToRoutes(store.state.menuItems)
                   const routes = menusToRoutes(frontRoutes)
                     // 动态添加路由
                     router.addRoutes(routes)
