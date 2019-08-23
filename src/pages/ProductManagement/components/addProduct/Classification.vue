00759450578a5e84b69bc94fb5c5d45e550cecbe
XiaoCai20180701 <421586311@qq.com>
2019-08-23 13:33:17 +0800

    修改 厂商 为 厂家

--- src/pages/ProductManagement/components/addProduct/Classification.vue
+++ src/pages/ProductManagement/components/addProduct/Classification.vue
@@ -16,7 +16,7 @@
       <Input v-model="productName" placeholder="请输入商品名称" clearable :disabled="disabled" required/>
     </div>
     <div class="tej-product-box">
-      <p><span class="symbol">*</span>所属厂商</p>
+      <p><span class="symbol">*</span>所属厂家</p>
       <Select v-model="vendor"
               @on-change="vendorChange"
               :disabled="disabled">
@@ -26,19 +26,39 @@
           :label="item.area"
         >
           <Option v-for="(child, c) in item.children" :value="child.vendorId" :key="child.vendorId">{{ child.vendorName
-            }} ({{child.vendorId}})</Option>
+            }} ({{child.vendorId}})
+          </Option>
         </OptionGroup>
       </Select>
+    </div>
+    <div class="tej-product-box">
+      <p><span class="symbol">*</span>商品货号</p>
+      <Input v-model="productNo" placeholder="请输入商品货号" clearable :disabled="disabled" required/>
+    </div>
+    <div class="tej-product-box">
+      <Button type="primary" @click="openModal">商品参数填写</Button>
+      <Modal
+        v-model="showModal"
+        title="Common Modal dialog box title"
+        width="1000"
+        @on-ok="ok"
+        @on-cancel="cancel">
+        <ParamsModal :params-obj="paramsObj"></ParamsModal>
+      </Modal>
     </div>
   </Card>
 </template>
 
 <script>
-  import {getVendorList} from '@/api/api'
-  import bus  from '@/utils/bus'
+  import {getVendorList, postSpecByTypeId} from '@/api/api'
+  import bus from '@/utils/bus'
+  import ParamsModal from './ParamsModal'
 
   export default {
     name: 'Classification',
+    components: {
+      'ParamsModal': ParamsModal
+    },
     props: {
       list: Array,
       classification: {
@@ -53,12 +73,16 @@
     data() {
       return {
         productName: '',
+        productNo: '',
         cascader: [],
         vendorList: [],
         vendor: null,
-        typeChildId: null,
+        typeChildId: null,      //商品分类二级目录Id
+        typeId: null,           //商品分类一级目录Id
         productVendorId: null,
-        openNum: 1
+        openNum: 1,
+        showModal: false,
+        paramsObj: {}
       }
     },
     mounted() {
@@ -70,13 +94,34 @@
       }
     },
     methods: {
+      getParams() {
+        postSpecByTypeId({
+          typeId: this.typeId
+        }).then(res => {
+          console.log('res!!!!!!!!!', res)
+          this.paramsObj = res.data
+          console.log('data!!!!!!!!!!!!!!!!!!!!!', this.paramsObj)
+        }).catch(res => {
+          this.$Message.error('获取商品规格参数模板失败')
+        })
+      },
+      openModal() {
+        this.showModal = true
+        this.getParams()
+      },
+      ok() {
+        this.showModal = false
+      },
+      cancel() {
+        this.showModal = false
+      },
       editInitData() {
         this.productName = this.classification.productName
         this.vendor = this.classification.vendorId
         this.cascader = [this.classification.typeParentId, this.classification.typeChildId]
       },
       vendorChange(value) {
-//        console.log('选择的厂商 productVendorId', value)
+//        console.log('选择的厂家 productVendorId', value)
         //TODO 下拉框需要支持搜索功能
         this.productVendorId = value
         this.$emit('classification-callback', {
@@ -87,6 +132,7 @@
       },
       classificationChange(value) {
         this.typeChildId = value[1]
+        this.typeId = value[0]
 //        console.log('选择商品分类 typeChildId', this.typeChildId)
         //传递给父级组件
         this.$emit('classification-callback', {
@@ -95,9 +141,9 @@
           productName: this.productName
         })
         //将typeChildId传递给兄弟组件(feature)，尺码新增需要typeChildId
-        bus.$emit('classification-brother-callback',value[1])
+        bus.$emit('classification-brother-callback', value[1])
       },
-      //获取厂商列表
+      //获取厂家列表
       getVendorList() {
         getVendorList('').then(res => {
           if (res.code != 200) {
@@ -108,7 +154,7 @@
           console.log('vendorList', this.vendorList)
         })
           .catch(err => {
-            this.$Message.error('获取厂商列表失败', err)
+            this.$Message.error('获取厂家列表失败', err)
           })
       }
     }
