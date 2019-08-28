58a5666ebf3723aeff07375bbf0c6190228ae31a
XiaoCai20180701 <421586311@qq.com>
2019-08-28 14:58:40 +0800

    厂家列表下拉框组件

---
+++ src/components/Vendor.vue
@@ -0,0 +1,40 @@
+<template>
+  <Select v-model="vendor"
+          @on-change="vendorChange"
+          :disabled="disabled">
+    <OptionGroup
+          v-for="(item, index) in vendorList"
+          :key="index"
+          :label="item.area">
+        <Option v-for="(child, c) in item.children" :value="child.vendorId" :key="child.vendorId">{{ child.vendorName}} ({{child.vendorId}})</Option>
+    </OptionGroup>
+  </Select>
+</template>
+
+<script>
+  export default {
+    name: 'Vendor',
+    props: {
+      vendorList: Array,
+      disabled: {
+        type: Boolean,
+        required: false,
+        default: false
+      }
+    },
+    data(){
+      return {
+        vendor: null
+      }
+    },
+    methods: {
+      vendorChange(val) {
+        this.$emit('vendor-change-callback', val)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
