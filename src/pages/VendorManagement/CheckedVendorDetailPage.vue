7f93e9ed87af1e0c042b04728dc9af4280c27cd0
XiaoCai20180701 <421586311@qq.com>
2019-08-29 16:58:49 +0800

    新增 厂家操作版的搜索区域

--- src/pages/VendorManagement/CheckedVendorDetailPage.vue
+++ src/pages/VendorManagement/CheckedVendorDetailPage.vue
@@ -1,45 +1,62 @@
 <template>
   <div class="tej-main">
     <Row :gutter="18">
-      <Col span="5">
+      <i-col span="5">
         <BaseInfo :detail-data="vendorDetail.vendorInfo"></BaseInfo>
-      </Col>
-      <Col span="19">
+      </i-col>
+      <i-col span="19">
         <div class="tej-vendor-detailinfo">
           <Row :gutter="20">
-            <Col span="12">
-                <DataInfo :data="vendorDetail.trafficAndSales" :is-vendor="isVendor"></DataInfo>
-            </Col>
-            <Col span="12">
+            <i-col span="12">
+              <DataInfo :data="vendorDetail.trafficAndSales" :is-vendor="isVendor"></DataInfo>
+            </i-col>
+            <i-col span="12">
               <EnvironmentImg :img-list="vendorDetail.vendorEnvironmentImg"></EnvironmentImg>
-            </Col>
+            </i-col>
+          </Row>
+          <!-- 厂家版，显示搜索区域-->
+          <Row v-if="!showSerach" class="search">
+            <i-col span="8">
+              <Input type="text"
+                     v-model="keywords"
+                     clearable
+                     @on-change="keywordsChange"
+                     placeholder="请输入商品货号或商品名称"
+              />
+            </i-col>
+            <i-col span="2" offset="1">
+              <Button type="primary" @click="searchClick">搜索</Button>
+            </i-col>
           </Row>
           <div class="tej-vendor-table">
-            <VendorTable  :show-loading="showLoading"
-                      :columns-data="columnsData"
-                      :table-data="tableData"
-                      :page="page"
-                      @page-change-callback="pageChange"
-                      @pageSize-change-callback="pageSizeChange"
+            <VendorTable :show-loading="showLoading"
+                         :columns-data="columnsData"
+                         :table-data="tableData"
+                         :page="page"
+                         @page-change-callback="pageChange"
+                         @pageSize-change-callback="pageSizeChange"
             >
             </VendorTable>
           </div>
         </div>
-      </Col>
+      </i-col>
     </Row>
   </div>
 </template>
 
 <script>
   import {postVendorDetail} from '@/api/api'
+  import {userType, vendorTable, vendorPlatformTable} from '@/api/tableData'
   import VendorTable from './components/VendorTable'
   import BaseInfo from '@/components/vendorRetail/BaseInfo'
   import DataInfo from '@/components/vendorRetail/DataInfo'
   import EnvironmentImg from '@/components/vendorRetail/EnvironmentImg'
+  import ICol from "../../../node_modules/iview/src/components/grid/col.vue";
 
   export default {
     name: 'CheckedVendorDetailPage',
     components: {
+      ICol,
       'VendorTable': VendorTable,
       'BaseInfo': BaseInfo,
       'DataInfo': DataInfo,
@@ -50,40 +67,7 @@
         showLoading: false,
         productShow: 0,  //未启用
         environmentImgIndex: 0,
-        columnsData: [
-          {
-            'title': '编号',
-            'key': 'id'
-          },
-          {
-            'title': '商品类别',
-            'key': 'typeName'
-          },
-          {
-            'title': '商品名称',
-            'key': 'name'
-          },
-          {
-            'title': '零售价',
-            'key': 'priceSale'
-          },
-          {
-            'title': '库存',
-            'key': 'inventory'
-          },
-          {
-            'title': '访问量',
-            'key': 'access'
-          },
-          {
-            'title': '销量',
-            'key': 'numberSale'
-          },
-          {
-            title: '操作',
-            slot: 'action',
-          }
-        ],
+       // columnsData: [],
         tableData: [],
         page: {
           index: 1,
@@ -94,13 +78,31 @@
           vendorInfo: {},
           trafficAndSales: {}
         },
-        isVendor: true
+        isVendor: true,
+        keywords: ''
+      }
+    },
+    computed: {
+      showSerach() {
+        let checked = localStorage.getItem('userType') == userType.platform
+        return checked ? true : false
+      },
+      columnsData(){
+        let checked = localStorage.getItem('userType') == userType.platform
+        return checked ? vendorPlatformTable : vendorTable
       }
     },
     mounted() {
       this.getList()
     },
     methods: {
+      keywordsChange(e) {
+        this.keywords = e.target.value
+      },
+      searchClick() {
+        //厂家版搜索
+        this.getList()
+      },
       pageChange(page) {
         this.page = page
         this.getList()
@@ -113,13 +115,14 @@
         this.showLoading = true
         postVendorDetail({
           vendorId: this.$route.params.vendorId,
-          page: 1,
-          pageSize: 10
+          page: this.page.index,
+          pageSize: this.page.size,
+          keywords: this.keywords
         }).then(res => {
           this.showLoading = false
-          if(res.code != 200){
+          if (res.code != 200) {
             this.$Message.warning(res.msg)
-            if(res.code === 9998){
+            if (res.code === 9998) {
               localStorage.clear()
               this.$router.push({path: '/login'})
             }
@@ -127,7 +130,7 @@
           }
           let data = res.data
           this.vendorDetail = data
-          console.log('data',data)
+          console.log('data', data)
           this.tableData = data.productInfo.productList
           this.page = {
             index: data.productInfo.page,
@@ -136,7 +139,7 @@
           }
         }).catch(err => {
           this.showLoading = false
-          this.$Message.error('获取厂商详情失败', err)
+          this.$Message.error('获取厂家详情失败', err)
         })
       }
     }
@@ -148,14 +151,18 @@
     /*padding: 12px;*/
   }
 
-
-
   .environment-imglist {
     background: #fff;
   }
 
   .tej-vendor-table {
     margin-top: 15px;
+    background: #fff;
+  }
+
+  .tej-vendor-detailinfo .search {
+    margin: 15px 0 5px 0;
+    padding: 8px;
     background: #fff;
   }
 </style>
