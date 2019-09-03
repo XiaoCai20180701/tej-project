bdfc25da5a6e982ccc3ca25f9562b082d578c9ae
XiaoCai20180701 <421586311@qq.com>
2019-09-03 09:55:17 +0800

    时间轴组件，用于物流信息  Init commit

---
+++ src/components/TejTimeline.vue
@@ -0,0 +1,33 @@
+<template>
+  <Timeline>
+    <TimelineItem v-for="(item, index) in list" :key="index" :color="color(index)">
+      <p class="time">{{item.ftime}}</p>
+      <p class="content">{{item.context}}</p>
+    </TimelineItem>
+  </Timeline>
+</template>
+
+<script>
+  export default {
+    name: 'TejTimeline',
+    props: {
+      list: Array
+    },
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+      color(){
+        return function (index) {
+          return index == 0 ? 'green': 'blue'
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
