faa01d6af6cba415878fce7c8f251f8855a135f9
XiaoCai20180701 <421586311@qq.com>
2019-09-05 12:35:13 +0800

    营销管理 优惠券详情页面  init commit

--- src/pages/MarketingManagement/CouponDetail.vue
+++ src/pages/MarketingManagement/CouponDetail.vue
@@ -1,90 +1,56 @@
 <template>
   <div>
-      <div style="background: white; padding: 15px;">
-        <p class="title_top">优惠劵基本信息</p>
-        <Row style="margin-top: 25px;">
-          <Form :label-width="120">
-            <i-col :xs="12" :sm="12" :md="6" :lg="6">
-              <FormItem label="发行商家：" >
-                <p>{{couponInfo.vendorName}}</p>
-              </FormItem>
-            </i-col>
-            <i-col :xs="12" :sm="12" :md="6" :lg="6">
-              <FormItem label="优惠劵名称：" >
-                <p>{{couponInfo.couponName}}</p>
-              </FormItem>
-            </i-col>
-            <i-col :xs="12" :sm="12" :md="6" :lg="6">
-              <FormItem label="优惠折扣：" style="width: 100%">
-                <span>满</span><span style="color: orangered;">{{couponInfo.couponThreshold}}元</span>
-                <span>减</span><span style="color: orangered;">{{couponInfo.couponPrice}}元</span>
-              </FormItem>
-            </i-col>
-            <i-col :xs="12" :sm="12" :md="6" :lg="6">
-              <FormItem label="发放数量：" >
-                <p>{{couponInfo.number}}张</p>
-              </FormItem>
-            </i-col>
-            <i-col :xs="12" :sm="12" :md="6" :lg="6">
-              <FormItem label="领取数量：" >
-                <p>{{couponInfo.receiveNum}}张</p>
-              </FormItem>
-            </i-col>
-            <i-col :xs="12" :sm="12" :md="6" :lg="6">
-              <FormItem label="已使用数量：">
-                <p>{{couponInfo.useNum}}张</p>
-              </FormItem>
-            </i-col>
-            <i-col :xs="12" :sm="12" :md="6" :lg="6">
-              <FormItem label="使用期限：" >
-                <p><span>{{couponInfo.startTime}}</span> - <span>{{couponInfo.endTime}}</span></p>
-              </FormItem>
-            </i-col>
-            <i-col :xs="12" :sm="12" :md="6" :lg="6">
-              <FormItem label="优惠劵状态：">
-                <p :style="getStatusStyle(couponInfo.status)">{{couponInfo.status}}</p>
-              </FormItem>
-            </i-col>
-          </Form>
-        </Row>
-      </div>
-      <div style="background: white; padding: 15px;margin-top: 20px;">
-        <div style="margin-top: 20px;">
-          <span class="title_top">可用优惠商品</span>
-          <Button icon="md-add" style="float: right;display: inline-block;" type="primary" @click="modal = true" v-if="isAdmin != 1">
-            继续添加商品</Button>
-          <Modal class="tej-modal" v-model="modal" :closable="false" :mask-closable="false" width=80vw>
-            <div v-if="modal == true">
-              <AddCouponProduct style="padding: 10px;min-height: 500px;max-height: 800px;overflow: auto;"></AddCouponProduct>
-            </div>
-            <div slot="footer" style="text-align: center;padding: 20px;">
-              <Button class="b_style" @click="modalCancel">取消</Button>
-              <Button class="b_style" style="color: white; background: #409EFF;margin-left: 25px;" @click="modalOk">确认
-              </Button>
-            </div>
-          </Modal>
-        </div>
-        <div style="width: 100%;">
-          <Table border :columns="columns" :data="data" >
-        </Table>
-        </div>
-      </div>
+    <Row :gutter="15">
+      <i-col span="6">
+        <CouponInfo :is-detail="isDetail"
+                    :coupon-info="couponInfo"
+        ></CouponInfo>
+      </i-col>
+      <i-col span="18">
+        <div style="background: white; padding: 15px;">
+            <span class="title_top">可用优惠商品</span>
+            <Button icon="md-add" style="float: right;display: inline-block;" type="primary" @click="modal = true" v-if="isAdmin != 1">
+              继续添加商品</Button>
+            <Modal class="tej-modal" v-model="modal" :closable="false" :mask-closable="false" width=80vw>
+              <div v-if="modal == true">
+                <AddCouponProduct style="padding: 10px;min-height: 500px;max-height: 800px;overflow: auto;"
+                                  :is-detail="true"
+                                  :coupon-id="couponInfo.couponId"
+                                  @coupon-product-list-callback="couponProductListCallback"
+                ></AddCouponProduct>
+              </div>
+              <div slot="footer" style="text-align: center;padding: 20px;">
+                <Button class="b_style" @click="modalCancel">取消</Button>
+                <Button class="b_style" style="color: white; background: #409EFF;margin-left: 25px;" @click="modalOk">确认
+                </Button>
+              </div>
+            </Modal>
+          </div>
+          <div style="width: 100%;">
+            <Table border :columns="columns" :data="list" >
+            </Table>
+          </div>
+      </i-col>
+    </Row>
+
   </div>
 </template>
 
 <script>
-  import AddCouponProduct from './AddCouponProduct'
-  import { StytemRole, CouponDetailTable } from '@/api/tableData'
-  import { postCouponDetails } from '@/api/api'
+  import AddCouponProduct from './components/AddCouponProduct'
+  import CouponInfo from './components/CouponInfo'
+  import { userType, CouponDetailTable } from '@/api/tableData'
+  import { postCouponDetails, postCouponRelation } from '@/api/api'
 
   export default {
     name: "CouponDetail",
     components: {
-      AddCouponProduct
+      AddCouponProduct,
+      CouponInfo
     },
     data() {
       return {
-        isAdmin: StytemRole.back,
+        isAdmin: userType.platform,
         modal: false,
         status: 1,
         page: {
@@ -93,9 +59,11 @@
           total: 10
         },
         columns: [],
-        data: [],
+        list: [],
         couponInfo: {},  //优惠券基本信息
-        style: ''
+        style: '',
+        isDetail: true,
+        productIds: []
       }
     },
     computed: {
@@ -111,23 +79,59 @@
       this.getCouponDetail()
     },
     methods: {
+      couponProductListCallback(data){
+        console.log('关联优惠券',data)
+        this.productIds = data.productIdList
+      },
       modalOk() {
-        // console.log('ok value', this.value)
-        this.modal = false
+        //优惠券没有更改，那么只关闭弹窗；有更改时，才调用关联优惠券接口
+        if(this.productIds.length == 0){
+          this.modal = false
+        }else {
+          this.couponRelationSumbit(this.productIds)
+        }
       },
       modalCancel() {
         console.log('cancel')
         this.modal = false
       },
+      couponRelationSumbit(productIds){
+        let params = {
+          couponId: this.couponInfo.couponId,
+          productIds: productIds
+        }
+        postCouponRelation(params).then(res => {
+          if(res.code != 200){
+            this.$Message.error(res.msg)
+            if(res.code === 9998){
+              localStorage.clear()
+              this.$router.push({path: '/login'})
+            }
+            return
+          }
+          this.modal = false
+          this.getCouponDetail()
+        }).catch(err => {
+          this.$Message.error('关联优惠券失败' + err)
+        })
+      },
       getCouponDetail(){
         let params = {
           page: this.page.index,
           pageSize: this.page.size,
-          couponId: this.$route.params.couponId
+          couponId: this.$route.params.couponId || this.couponInfo.couponId
         }
         postCouponDetails(params).then(res => {
+          if(res.code != 200){
+            this.$Message.error(res.msg)
+            if(res.code === 9998){
+              localStorage.clear()
+              this.$router.push({path: '/login'})
+            }
+            return
+          }
           let data = res.data
-          this.data = data.productInfo.list
+          this.list = data.productInfo.list
           this.couponInfo = data.couponInfo
         }).catch(err => {
           this.$Message.error('获取优惠券详情失败' + err)
