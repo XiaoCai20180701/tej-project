09302b1432a696a2112479ca1f70cab8f558cc35
XiaoCai20180701 <421586311@qq.com>
2019-09-21 12:54:49 +0800

    自定义 级联选择器 init commit

---
+++ src/components/TejCascader.vue
@@ -0,0 +1,43 @@
+<template>
+  <Cascader :data="data"
+            v-model="cascader"
+            change-on-select
+            @on-change="onChange"
+  ></Cascader>
+</template>
+
+<script>
+  export default {
+    name: 'TejCascader',
+    props: {
+      data: Array,
+      cascaderModel: {
+        type: Array,
+        required: false
+      }
+    },
+    data() {
+      return {
+        cascader: []
+      }
+    },
+    computed: {
+
+    },
+    mounted(){
+      console.log('cascaderModel',this.cascaderModel)
+      this.cascader = this.cascaderModel
+    },
+    methods: {
+      onChange(value, selectedData) {
+        console.log('级联选择',value, selectedData)
+        this.$emit('cascader-callback',value)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+
